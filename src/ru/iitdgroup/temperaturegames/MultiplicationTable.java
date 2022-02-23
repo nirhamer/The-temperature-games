835cc8d0ae022259ba55750c47193301fcfea40e
nir <nirhamer17@walla.co.il>
2022-02-23 12:48:53 +0200

    multiplication table V1.3 now working as expected

--- src/ru/iitdgroup/temperaturegames/MultiplicationTable.java
+++ src/ru/iitdgroup/temperaturegames/MultiplicationTable.java
@@ -2,8 +2,14 @@ package ru.iitdgroup.temperaturegames;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
+        printHalfOfTheTable(1);
+        System.out.println();
+        printHalfOfTheTable(6);
+    }
+
+    public static void printHalfOfTheTable(int startFrom) {
         for (int line = 1; line <= 10; line++) {
-            for (int i = 1; i <= 10; i += 1) {
+            for (int i = startFrom; i < startFrom + 5; i += 1) {
                 System.out.print(i + "x" + line + "=" + (i * line) + "\t");
             }
             System.out.println();
