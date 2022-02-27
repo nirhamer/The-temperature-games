b47d1bb3f6b08710ea8c2eed248b6aa767b92c10
nir <nirhamer17@walla.co.il>
2022-02-27 19:08:57 +0200

    progress

--- src/ru/iitdgroup/temperaturegames/SimpleJustify.java
+++ src/ru/iitdgroup/temperaturegames/SimpleJustify.java
@@ -2,20 +2,39 @@ package ru.iitdgroup.temperaturegames;
 
 public class SimpleJustify {
     public static void main(String[] args) {
-        for (int i =0; i<3; i++){
+        for (int i = 0; i < 3; i++) {
             partTwo();
             System.out.println("\n");
         }
     }
-    public static void partTwo(){
+
+    public static void partTwo() {
         String text = "Sabers have a base damage multiplier of 1.0x.";
         String[] separated = text.split(" ");
-
+        int maxLen = findLongestElement(separated);
         for (String word : separated) {
-            if (!word.trim().isEmpty()) {
-                String padded = String.format("%"+word.length()+"s", "").replace('*', ' ');
-                System.out.println(word + padded);
+            System.out.println(getSpaces(word, maxLen) + word);
+
+        }
+    }
+
+    private static int findLongestElement(String[] separated) {
+        int max = 0;
+        for (String currentElement : separated) {
+            int currentLength = currentElement.length();
+            if (currentLength > max) {
+                max = currentLength;
             }
         }
+        return max;
+    }
+
+    private static String getSpaces(String word, int maxLen) {
+        int difference = maxLen - word.length();
+        String a = "";
+        for (int i = 0; i < difference; i++) {
+            a = a + " ";
+        }
+        return a;
     }
 }
